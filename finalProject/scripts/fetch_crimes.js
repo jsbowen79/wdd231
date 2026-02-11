fc38fa16321e511bcf3febdf79c39ae87b9716c5
jsbowen79 <jsbowen79@outlook.com>
2026-02-11 15:39:32 -0700

    Test Cors2

--- finalProject/scripts/fetch_crimes.js
+++ finalProject/scripts/fetch_crimes.js
@@ -1,34 +1,29 @@
 export async function fetchCrimeData(recordCount) {
     const url =
-        "https://data.denvergov.org/resource/3nai-9n6v.json?$limit=100&$order=reported_date DESC"
-    ;
-  
-  
-       
+        "https://api.allorigins.win/raw?url=" +
+        encodeURIComponent("https://data.denvergov.org/resource/3nai-9n6v.json?$limit=100");
+
         
-    try {
-        async function test() {
-            const url = "https://data.denvergov.org/resource/3nai-9n6v.json?$limit=5";
+        
+        
+        
+        
+        try {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => console.log(data));
 
-            const response = await fetch(url);
-            const data = await response.json();
-
-            console.log(data);
+      
+        if (!response.ok) {
+            throw new Error("Network Response Failed"); 
         }
 
-        test();
-          
-      
-        // if (!response.ok) {
-        //     throw new Error("Network Response Failed"); 
-        // }
-
-        // const data = await response.json(); 
-        // console.log(data); 
-        // if (!Array.isArray(data.features)) {
-        //     console.error("ArcGIS returned unexpected payload:", data); 
-        // }
-        // return Array.isArray(data.features) ? data.features : []; 
+        const data = await response.json(); 
+        console.log(data); 
+        if (!Array.isArray(data.features)) {
+            console.error("ArcGIS returned unexpected payload:", data); 
+        }
+        return Array.isArray(data.features) ? data.features : []; 
 
     } catch (error) {
         console.error("Failed to Load Crime Data")
